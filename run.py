95b4843b2b20dad54306f3c3ec3b2f218a9b3104
yihan.mo@outlook.com <yihan.mo@outlook.com>
2021-09-19 21:59:57 -0400

    add time route

--- run.py
+++ run.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from datetime import datetime
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello world!'
+
+@app.route('/time')
+def get_time():
+    return str(datetime.now())
 
 
 app.run(host='0.0.0.0',
